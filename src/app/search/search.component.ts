3e36d6faa228962bdb3aee1178bc0721538b3a43
Steve-njeri <stevenjeri254@gmail.com>
2021-07-26 22:04:33 +0300

    added new file  component app/repository.ts  and modified app.search/search.component.ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UserRequestApiService } from '../user-http/user-request-api.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [UserRequestApiService],
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+    
+  constructor(private userRequestService:UserRequestApiService) { }
 
   ngOnInit(): void {
   }
