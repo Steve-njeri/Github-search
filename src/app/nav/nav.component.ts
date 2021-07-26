a9a9c3ebac84b5e3fd5b4112385999968adfaf5b
Steve-njeri <stevenjeri254@gmail.com>
2021-07-26 17:57:22 +0300

    modified nav-component and add bootstrap

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  logo = 'Git-Search';
   constructor() { }
 
   ngOnInit(): void {
