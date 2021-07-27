3b2f9992e0856f00eec37843c5cb77a67f5d9b3a
Steve-njeri <stevenjeri254@gmail.com>
2021-07-27 03:53:03 +0300

    modified package-lock.json, package.json, environment.ts and tsconfig.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   apiUrl:"https://api.github.com",
-  clientId:"22064d2730dbc8e29879",
-  clientSecret: "edc18bd1ee5d8a9b3ffe9951acdf17d63fe8d0bf"
+  clientId:"abeb90549ecafe81772b",
+  clientSecret: "045cee6191464590b656b150087fd37724d5454c"
 };
 
 /*
